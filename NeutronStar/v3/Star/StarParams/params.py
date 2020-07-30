09f6ccb74dca400d2be30583147b20050e229f06
Demon-of-Asgard <kuttan.mgc@gmail.com>
2020-07-30 18:38:31 +0800

    Changed Diff eq solver from Euler to 4th order Runge-Kutta.

--- NeutronStar/v3/Star/StarParams/params.py
+++ NeutronStar/v3/Star/StarParams/params.py
@@ -6,7 +6,8 @@ except ImportError:
 
 class Params:
     def __init__(self):
-
+        
+        # Constant values. 
         self.__pi = np.pi
         self.__c = 3.0e10  # [cm/s]
         self.__Ms = 1.989e33  # [gm]
@@ -14,16 +15,18 @@ class Params:
         self.__hbar = self.__h/(2.0*self.__pi)  # [erg s]
         self.__mn = 1.6749e-24  # [gm]
         self.__me = 9.10e-28  # [gm]
+        self.__R0 = 1.476  # [km]
 
+        #Model dependent choices.
         self.__A = 2.15
         self.__Z = 1.0
-        self.__R0 = 1.476  # [km]
         self.__alpha_rl = self.__R0
         self.__gamma_rl = 4./3.
         self.__gamma_nrl = 5./3.
         self.__A_rl = 2.8663
         self.__A_nrl = 2.4216
 
+        #Initial values.
         self.__barM0 = 0.0
         self.__barP0 = 0.01
         self.__dr = 0.01
@@ -47,24 +50,15 @@ class Params:
         A_rl = self.__A_rl
         A_nrl = self.__A_nrl
 
-
-        #Krl = (hbar*c/(12.*pi**2))*(3.0*pi**2*Z/(A*mn*c**2))**gamma_rl
-        #e0_rl = ((R0/alpha_rl)**gamma_rl/Krl)**(1/(gamma_rl-1))
         e0_rl = mn**4*c**5/(3.*pi**2*hbar**3)
-        #print(e0_rl)
-        # barKrl = Krl*e0_rl**(gamma_rl-1)
-        # beta_rl = 1.0e15*(4.0*pi*e0_rl)/(Ms*c**2*barKrl**(1/gamma_rl))
-
+     
         dict_params = {
             "c":self.__c,
             "pi":self.__pi,
             "R0":self.__R0,
             "Ms":self.__Ms,
-            #"Krl": Krl,
             "e0_rl" : e0_rl,
-            #"barKrl": barKrl,
             "alpha_rl": alpha_rl,
-            #"beta_rl" : beta_rl,
             "gamma_rl" : gamma_rl,
             "gamma_nrl":gamma_nrl,
             "A_rl": A_rl,
@@ -75,16 +69,17 @@ class Params:
     
     def init_params(self):
         r0 = self.__r0
+        dr = self.__dr
         barP0 = self.__barP0
         barM0 = self.__barM0
 
         init_params = {
 
             "r0":r0,
+            "dr":dr,
             "barP0":barP0,
             "barM0":barM0,
         }
-
         return init_params
 
     #-------------------------------------------------------------
--- NeutronStar/v3/Star/starGenerator.py
+++ NeutronStar/v3/Star/starGenerator.py
@@ -9,12 +9,15 @@ from Plot.plot import Plot as Plot
 #-----------------------------------------------------------------------------------
 
 class NS(Eos):
-    ''' Calss representing a Neutron star. Inherits properties of class Eos,
-    which inherits properties from class Params.'''
+    ''' 
+    Calss representing a Neutron star. Inherits properties of class Eos,
+    which inherits properties from class Params.
+    '''
 
     barP = []
     barM = []
     r = []
+    dr = 0.0
 
     #-----------------------------------------------------------------------------------
 
@@ -24,6 +27,7 @@ class NS(Eos):
         self.barP.append(init_vals["barP0"])
         self.barM.append(init_vals["barM0"])
         self.r.append(init_vals["r0"])
+        self.dr = init_vals["dr"]
 
     #-----------------------------------------------------------------------------------
     def get_init_vals(self):
@@ -37,11 +41,13 @@ class NS(Eos):
 
         barP0 = init_params["barP0"]
         r0 = init_params["r0"]
+        dr = init_params["dr"]
 
         barM0 = (4.*pi*e0_rl/(3.*Ms*c**2))*r0**3.*self.eos(barP0) 
 
         init_vals = {
             "r0":r0,
+            "dr":dr,
             "barP0":barP0,
             "barM0":barM0,
         }
@@ -52,7 +58,9 @@ class NS(Eos):
     #-----------------------------------------------------------------------------------
 
     def print_params(self):
-        ''' To cross check values of the star-parameters.'''
+        ''' 
+        To cross check values of the star-parameters.
+        '''
 
         params = self.get_params()
         for index in params:
@@ -60,17 +68,14 @@ class NS(Eos):
 
     #-----------------------------------------------------------------------------------
 
-    def dbarM_dr(self):
+    def dbarM_dr(self, r, barM, barP, params):
         ''' RHS of the mass balance equation in the TOV eqns. '''
 
-        params = self.get_params()
+        
         Ms = params["Ms"]
         c = params["c"]
         pi = params["pi"]
         eps0 = params["e0_rl"]
-
-        barP = self.barP[-1]
-        r = self.r[-1]
 
         barE = self.eos(barP)
 
@@ -80,10 +85,9 @@ class NS(Eos):
         
     #-----------------------------------------------------------------------------------
 
-    def dbarP_dr(self):
+    def dbarP_dr(self, r, barM, barP, params):
         ''' RHS of the force balance eqn in the TOV eqns.'''
 
-        params = self.get_params()
         R0 = params["R0"]
         pi = params["pi"]
         c  = params["c"]
@@ -108,21 +112,45 @@ class NS(Eos):
     #-----------------------------------------------------------------------------------
 
     def build(self):
-        ''' Estimate the next value of bar P, barM and r by solving TOV eqns
-         using simple Euler method. The iteration of the while-loop terminates when barP
-         hits negative value. '''
+        ''' 
+        Estimate the next value of bar P, barM and r by solving TOV eqns
+        using simple Euler method. The iteration of the while-loop terminates when barP
+        hit negative value. 
+        '''
 
+        params = self.get_params()
         i = 0
-        dr = 0.1
+        dr = self.dr
         while(True and i<= 10000000):
 
             i += 1
-
-            barP_lst = self.barP[-1]
-            barP_nxt = barP_lst + self.dbarP_dr()*dr
-            barM_lst = self.barM[-1]
-            barM_nxt = barM_lst + self.dbarM_dr()* dr 
+            
             r_lst = self.r[-1]
+            barM_lst = self.barM[-1]
+            barP_lst = self.barP[-1]  
+            
+            '''
+            Runge-Kutta implementation. The differential eqn. are coupled. Runge-Kutta is implemented 
+            here only for one variable at a time per iteration. That is in each iteration, value of next 
+            barP is calculated assuming barM is a constant during that iteration. Similarely for other 
+            parameters.
+            '''
+
+            barPK1 = dr * self.dbarP_dr(r_lst, barM_lst, barP_lst, params)
+            barPK2 = dr * self.dbarP_dr(r_lst+(0.5*dr), barM_lst, barP_lst+(0.5*barPK1), params)
+            barPK3 = dr * self.dbarP_dr(r_lst+(0.5*dr), barM_lst, barP_lst+(0.5*barPK2), params)
+            barPK4 = dr * self.dbarP_dr(r_lst+dr, barM_lst, barP_lst+barPK3, params)
+
+            barMK1 = dr * self.dbarM_dr(r_lst, barM_lst, barP_lst, params)
+            barMK2 = dr * self.dbarM_dr(r_lst+(0.5*dr), barM_lst+(0.5*barMK1), barP_lst, params)
+            barMK3 = dr * self.dbarM_dr(r_lst+(0.5*dr), barM_lst +(0.5*barMK2), barP_lst, params)
+            barMK4 = dr * self.dbarM_dr(r_lst+dr, barM_lst+barMK3, barP_lst, params)
+
+            barP_nxt = barP_lst + (1.0/6.0)*(barPK1+(2.0*barPK2)+(2.0*barPK3)+barPK4)
+
+            barM_nxt = barM_lst + (1.0/6.0)*(barMK1+(2.0*barMK2)+(2.0*barMK3)+barMK4)
+
+            
             r_nxt = r_lst + dr
 
             if barP_nxt >= 0.0:
@@ -149,10 +177,10 @@ def main():
     star_obj = NS() # Instance of class NS
     star_obj.print_params() # cross check the params. 
     print("--------------------------------------------------\n")
-    outfanme = star_obj.build() # Build star
+    outfname = star_obj.build() # Build star
     print("--------------------------------------------------\n")
     plot_obj = Plot() # Used for plotting
-    plot_obj.plot(outfanme)
+    plot_obj.plot(outfname)
 
 
 #-----------------------------------------------------------------------------------
